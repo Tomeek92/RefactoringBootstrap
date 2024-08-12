d194e1525e934c7045d754d2e94180ad8a8e1d52
tfryckowski <tfryckowski@BYD-TFRYCKOWSKI>
2024-08-12 10:59:38 +0200

    Dodaj pliki projektów.

---
+++ CleanArchitectureBlazor.Domain/Accounts/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureBlazor.Domain.Admin
+{
+    public class Account
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string AdminName { get; set; } = null!;
+        [Required]
+        public string AdminEmail { get; set; } = null!;
+    }
+}
